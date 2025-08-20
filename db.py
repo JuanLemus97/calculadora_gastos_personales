fb85fd7d4e2deb2ae179c27d1ff5af64d133a90a
Juan Daniel Capilla Lemus <JuanLemus@iMac-de-Felipe.local>
2025-08-20 16:17:17 -0600

    Agregando base de datos

--- db.py
+++ db.py
@@ -3,9 +3,9 @@ from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 from datetime import datetime
 
-Base = declarative_base()
 engine = create_engine("postgresql://cal_gastos_db_user:5AGCwr9zv0w7ypN415NkFW2ve5KSYPqw@dpg-d2j2ulogjchc73fp20a0-a/cal_gastos_db")
 Session = sessionmaker(bind=engine)
+Base = declarative_base()
 
 class Gasto(Base):
     __tablename__ = "gastos"
