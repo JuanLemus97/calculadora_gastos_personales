c660172ddbe1d545f498c12c49d59c7ccba1564d
Juan Daniel Capilla Lemus <JuanLemus@iMac-de-Felipe.local>
2025-08-20 16:02:21 -0600

    Agregando base de datos

--- db.py
+++ db.py
@@ -1,19 +1,18 @@
-from sqlalchemy import create_engine, Column, Integer, String, Float
-from sqlalchemy.orm import declarative_base, sessionmaker
-import os
+from sqlalchemy import create_engine, Column, Integer, String, Float, DateTime
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.ext.declarative import declarative_base
+from datetime import datetime
 
-DATABASE_URL = os.getenv("DATABASE_URL", "sqlite:///local.db") #Fallback para desarrollo
-
-engine = create_engine(DATABASE_URL)
-Session = sessionmaker(bind = engine)
+engine = create_engine("postgresql://user:password@localhost/calculadora_gastos")
+Session = sessionmaker(bind=engine)
 Base = declarative_base()
 
 class Gasto(Base):
     __tablename__ = "gastos"
-    id = Column(Integer, primary_key = True)
-    fecha = Column(String)
-    categoria = Column(String)
-    monto = Column(Float)
-    descripcion = Column(String)
+    id = Column(Integer, primary_key = True, autoincrement=True)
+    fecha = Column(DateTime, default = datetime.now)
+    categoria = Column(String, nullable=False)
+    monto = Column(Float, nullable=False)
+    descripcion = Column(String, nullable=True)
 
 Base.metadata.create_all(engine)
