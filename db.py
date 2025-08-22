97b379b789e4bb34078a6b980f6895b83085b506
Juan Daniel Capilla Lemus <JuanLemus@iMac-de-Felipe.local>
2025-08-22 16:35:12 -0600

    Agregando base de datos

--- db.py
+++ db.py
@@ -1,6 +1,5 @@
 from sqlalchemy import create_engine, Column, Integer, String, Float, DateTime
-from sqlalchemy.orm import sessionmaker
-from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker, declarative_base
 from datetime import datetime
 
 engine = create_engine("postgresql://cal_gastos_db_user:5AGCwr9zv0w7ypN415NkFW2ve5KSYPqw@dpg-d2j2ulogjchc73fp20a0-a/cal_gastos_db")
